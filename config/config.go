79c7d739c9fda545c946cdb7986dd0eca874b9f3
Adam Chalkley <atc0005@users.noreply.github.com>
2019-10-28 22:47:28 -0500

    Set godoc description for config package

--- config/config.go
+++ config/config.go
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package config provides types and functions to collect, validate and apply
+// user-provided settings.
 package config
 
 import (
